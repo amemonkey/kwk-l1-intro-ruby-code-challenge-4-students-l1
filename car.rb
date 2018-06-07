7e88d13d08bed46da4eb291c20cc08d78ccd6840
amemonkey <ameliafunaki789@gmail.com>
2018-06-07 22:16:00 +0000

    automatically backed up by learn

---
+++ car.rb
@@ -0,0 +1,31 @@
+class Car
+  def initialize(make, model, color)
+    @make = make
+    @model = model
+    @color = color
+  end
+  
+  def make
+    @make
+  end
+  
+  def make = (new_make)
+    @make = new_make
+  
+  def model
+    @model
+  end
+  
+  def model = (new_model)
+    @model = new_model
+  end
+  
+  def color
+    @color
+  end
+  
+  def color = (new_color)
+    @color = new_color
+  end
+  
+end
